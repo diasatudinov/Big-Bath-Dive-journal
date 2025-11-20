7505ed924e57bf00a7d659250510d70970204319
Dias <dias.atudinov@gmail.com>
2025-11-20 20:06:58 +0500

    new files

---
+++ Big Bath Dive journal/Big Bath Dive journal/BBHelpers/Date + ext.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Big Bath Dive journal
+//
+//  Created by Dias Atudinov on 20.11.2025.
+//
+
---
+++ Big Bath Dive journal/Big Bath Dive journal/BBModules/BBMyDives/BBDiveCell.swift
@@ -0,0 +1,18 @@
+//
+//  BBDiveCell.swift
+//  Big Bath Dive journal
+//
+//  Created by Dias Atudinov on 20.11.2025.
+//
+
+import SwiftUI
+
+struct BBDiveCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BBDiveCell()
+}
